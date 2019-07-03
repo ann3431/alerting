691c788d9d76d31d6242d5cd99daf19a7505cedc
Annie Lee <ann3431@hotmail.com>
2019-07-03 12:33:29 -0700

    Update HttpInputTest.kt

--- core/src/test/kotlin/com/amazon/opendistroforelasticsearch/alerting/core/model/HttpInputTest.kt
+++ core/src/test/kotlin/com/amazon/opendistroforelasticsearch/alerting/core/model/HttpInputTest.kt
@@ -6,11 +6,61 @@ import kotlin.test.assertEquals
 
 class HttpInputTest {
     // Test invalid url with different format in one function
-    fun `test invalid urls`() = try {
-        // Invalid scheme
-        val invalidSchemeHttpInput = HttpInput("htttttp", "localhost", 9200, "_cluster/health", null, null, "", 5000, 5000)
-        fail("Invalid scheme when creating HttpInput should fail.")
-    } catch (e: IllegalStateException) {
+    fun `test invalid urls`() {
+        try {
+            // Invalid scheme
+            val invalidSchemeHttpInput = HttpInput("htttttp", "localhost", 9200, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid scheme when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        try {
+            // Invalid host
+            val invalidHostHttpInput = HttpInput("http", "locohost", 9200, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid host when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        try {
+            // Invalid port
+            val invalidPortHttpInput = HttpInput("http", "localhost", -1, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid port when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        try {
+            // Invalid path
+            val invalidPathHttpInput = HttpInput("http", "localhost", 9200, "////////", null, null, "", 5000, 5000)
+            fail("Invalid path when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        /** try {
+            // Invalid params
+            val invalidParamsHttpInput = HttpInput("http", "localhost", 9200, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid params when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        try {
+            // Invalid body
+            val invalidBodyHttpInput = HttpInput("http", "localhost", 9200, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid body when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        try {
+            // Invalid url
+            val invalidUrlHttpInput = HttpInput("http", "localhost", 9200, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid url when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        try {
+            // Invalid connection timeout
+            val invalidConnectionTimeoutHttpInput = HttpInput("http", "localhost", 9200, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid connection timeout when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        }
+        try {
+            // Invalid socket timeout
+            val invalidSocketTimeoutHttpInput = HttpInput("http", "localhost", 9200, "_cluster/health", null, null, "", 5000, 5000)
+            fail("Invalid socket timeout when creating HttpInput should fail.")
+        } catch (e: IllegalStateException) {
+        } */
     }
     // Test valid url field by field
     fun `test valid url`() {
